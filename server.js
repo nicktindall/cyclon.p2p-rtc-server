3474adf66608b25a218172ef53a850653016cf9e
Nick Tindall <nick@tindall.id.au>
2014-06-22 15:22:37 +1000

    Initial commit

---
+++ server.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var http = require("http");
+var express = require("express");
+var io = require("socket.io");
+var bodyParser = require("body-parser");
+var PeerNotifier = require("./lib/PeerNotifier");
+var PeerApi = require("./lib/PeerApi");
+
+var DEFAULT_PORT = 2222;
+
+var app = express();
+app.use(bodyParser.json());
+
+//
+// Allow CORS for all resources
+//
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Content-Type");
+	next();
+});
+
+// Create the server
+var server = http.createServer(app);
+
+var socketio = io.listen(server);
+var peerNotifier = new PeerNotifier(socketio);
+new PeerApi(app, peerNotifier);
+
+//
+// Start the server
+//
+server.listen(process.env.PORT || DEFAULT_PORT);
+
