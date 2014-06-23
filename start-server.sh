0a86999f1d1251e6893e19f4df21aaf8bafe20ed
Nick Tindall <nick@tindall.id.au>
2014-06-23 21:06:04 +1000

    asdas

---
+++ start-server.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+if [ -n "$1" ]
+	then 
+	echo "Running on port $1"
+	export PORT=$1
+else
+	echo "Running on default port"
+fi
+
+node ./node_modules/cyclon.p2p-webrtc-server/server.js
+
